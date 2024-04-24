94710451170562d02dd14681bdfb0200a13fd13b
trifonov.a <rsanddrs-company@yandex.ru>
2024-04-24 08:38:56 +0300

    Update
    .

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] List<Animator> CUObjects;
+    public void CloseUp(){
+    
+    foreach (Animator a in CUObjects){
+    a.SetBool("CU", true);
+    }
+    }
+}
