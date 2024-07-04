1a00be270cda42691b068674b954c931ce33bb86
andrei-trifonov <rsanddrs-company@yandex.ru>
2024-07-04 08:50:38 +0300

    var2
    .

--- Assets/PizdaBomjihi.cs
+++ Assets/PizdaBomjihi.cs
@@ -1,20 +1,49 @@
 using System.Collections;
-using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.UI;
+
 public class PizdaBomjihi : MonoBehaviour
 {
-    private VideoPlayer VP;
-    // Start is called before the first frame update
+    public VideoPlayer videoPlayer;  // Ссылка на Video Player
+    private bool Block;
+    private float videoDuration;
+
     void Start()
     {
-        VP = GetComponent<VideoPlayer>();   
+        // Получаем длительность видео
+        
+
+
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-         if (!VP.isPlaying)
-            VP.Play();        
+        // Обновляем таймер
+        float currentTime = (float)videoPlayer.time;
+       
+        Debug.Log("Time " +videoPlayer.time );
+        Debug.Log("Duration " + videoDuration );
+        // Проверяем, закончилось ли видео
+        videoDuration = (float)videoPlayer.clip.length;
+        if (videoPlayer.time >= videoDuration-0.5f && Block == false)
+        {
+            Block = true;
+            // Отключаем компонент Video Player
+            StartCoroutine(BomzhCoroutine());
+
+        }
     }
+
+    IEnumerator BomzhCoroutine()
+    {
+        videoPlayer.enabled = false;
+        yield return new WaitForSeconds(0.01f);
+        videoPlayer.enabled = true;
+        videoDuration = (float)videoPlayer.clip.length;
+        Block = false;
+    }
+    // Форматирование времени в формате "ММ:СС"
+    
 }
